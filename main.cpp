ec459e3d18f00ad7c85231d8ba3423dfb9cb1f5d
Kaczorek <kaczorowski0000@gmail.com>
2024-04-19 16:08:11 +0200

    base code

--- main.cpp
+++ main.cpp
@@ -1,49 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-main()
-{
-int i, aSize;
-int * randomNums;
-time_t t;
-double total = 0;
-int biggest, smallest;
-float average;
-srand(time(&t));
-printf("Ile liczb losowych chcesz zapisac w tablicy? \n ");
-scanf(" %d", &aSize);
-// alokacja tablicy tylu liczb całkowitych, ile zażądał użytkownik
-randomNums = (int *) malloc(aSize * sizeof(int));
-// sprawdzenie czy tablica została prawidłowo alokowana
-if (!randomNums)
-{
-printf("Nie udało się alokować tablicy!\n");
-exit(1);
+#include <iostream>
+#include "array.h"
+
+void menu(void){
+
+    printf("1.enter data\n");
+    printf("2. display the content of the array\n");
+    printf("3. detemine the minimum value \n");
+    printf("4. detemine the maximum value \n");
+    printf("5. detemine the average value \n");
+    printf("0.Exit\n");
+    printf("Select an option:\n");
 }
-// przegląda tablicę i przypisuje jej elementom
-// losowe liczby z przedziału od 1 do 500
-    for (i = 0; i < aSize; i++)
-    {
-        randomNums[i] = (rand() % 500) + 1;
-    }
-    biggest = 0;
-    smallest = 500;
-    for (i = 0; i < aSize; i++)
-    {
-        total += randomNums[i];
-        if (randomNums[i] > biggest)
-        {
-            biggest = randomNums[i];
+
+int main() {
+    int array[SIZE] = {1, 2, 3, -4, 50, 6, 7, 8, 9, 10};
+    printf("Simple array...\n");
+
+    int option = 0;
+    int max = findMaximumValue(array);
+    int min = findMinimumValue(array);
+    float avg = findSumValue(array);
+    do {
+        menu();
+        scanf("%d", &option);
+        switch (option) {
+            case 0:
+                break;
+            case 1:
+                wpisywanie(array);
+                break;
+            case 2:
+                displayArray(array);
+                break;
+            case 3:
+                findMinimumValue(array);
+                printf("wynik = %d\n", min);
+                break;
+            case 4:
+                findMaximumValue(array);
+                printf("wynik = %d\n", max);
+                break;
+            case 5:
+                findSumValue(array);
+                printf("wynik = %f\n", avg);
+                break;
+            default:
+                printf("Chose correct number...\n\n");
         }
-        if (randomNums[i] < smallest)
-        {
-            smallest = randomNums[i];
-        }
+    } while (option != 0);
+    {
+        printf("koniec : )\n");
     }
-    average = ((float)total)/((float)aSize);
-    printf("Najwieksza liczba: %d.\n", biggest);
-    printf("Najmniejsza liczba: %d.\n", smallest);
-    printf("Srednia: %.2f.\n", average);
-    free(randomNums);
-    return(0);
+    return 0;
 }
