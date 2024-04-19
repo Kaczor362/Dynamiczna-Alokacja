759d4f5850de3be00fc9550e0c25ea34fe099858
Kaczorek <kaczorowski0000@gmail.com>
2024-04-19 16:08:11 +0200

    base code

---
+++ array.cpp
@@ -0,0 +1,48 @@
+//
+// Created by student on 19.04.2024.
+//
+#include <iostream>
+#include "array.h"
+
+void displayArray(int array[]){
+    for (int i = 0; i < SIZE; ++i) {
+        printf("array [%d] = %d\n", i, array[i]);
+    }
+}
+int findMaximumValue(int array[]){
+    int max = array[0];
+    for (int i = 0; i < SIZE; ++i) {
+        if (array[i]>max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+int findMinimumValue(int array[]){
+    int min = array[0];
+    for (int i = 0; i < SIZE; ++i) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+int findSumValue(int array[])
+{
+    int sum = 0;
+
+    for (int i = 0; i < SIZE; ++i) {
+        sum += array[i];
+    }
+    float ave = (float) sum / SIZE;
+    return ave;
+}
+void wpisywanie(int array[])
+{
+    for (int i = 0; i < SIZE; ++i) {
+        printf("array[%d]=", i);
+        scanf("%d", &array[i]);
+    }
+}
+
