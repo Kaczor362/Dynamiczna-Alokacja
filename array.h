4435afe2fdac23b06ccb9d196358d0ed6bcd86e6
Kaczorek <kaczorowski0000@gmail.com>
2024-04-19 16:09:23 +0200

    adding array.h

---
+++ array.h
@@ -0,0 +1,19 @@
+
+#ifndef UNTITLED1XD_ARRAY_H
+#define UNTITLED1XD_ARRAY_H
+
+#define  SIZE 10
+
+
+void displayArray(int array[]);
+
+int findMaximumValue(int array[]);
+
+int findMinimumValue(int array[]);
+
+int findSumValue(int array[]);
+
+void wpisywanie(int array[]);
+
+
+#endif //UNTITLED1XD_ARRAY_H
